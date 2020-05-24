62fba643bda697c988c36d3d71180369180a4a0b
Ubuntu <ubuntu@CloudCompute>
2020-05-24 17:08:44 -0400

    Corrected input/output dimensions, added super call (wasn't able to test code at first commit...), add option to input an existing hidden state

--- STAR_Single.py
+++ STAR_Single.py
@@ -1,24 +1,30 @@
 import torch
 import torch.nn as nn
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class STAR_Single(nn.Module):
 	# A single layer STAR Network
 	def __init__(self,input_dim,hidden_dim):
+		super(STAR_Single,self).__init__()
+		self.hidden_dim = hidden_dim
 		self.project = nn.Sequential(nn.Linear(input_dim,hidden_dim), nn.Tanh())
 		self.gate = nn.Sequential(nn.Linear(input_dim+hidden_dim,hidden_dim),nn.Sigmoid())
 		self.tanh_out = nn.Tanh()
 	
-	def forward(self,x):
-		batch_size, seq_size, _,_,_ = x.size()
-		hidden_out = torch.zeros(self.hidden_dim,seq_size)
-		h_t = torch.zeros(self.hidden_dim,1)
+	def forward(self,x,h=None):
+		seq_size, batch_size,_ = x.size()
+		hidden_out = torch.zeros(seq_size,batch_size,self.hidden_dim).to(device)
+		if h == None:
+			h_t = torch.zeros(batch_size,self.hidden_dim).to(device)
+		else:
+			h_t = torch.squeeze(h)
 		for t in range(seq_size):
-			x_t = x[:,t,:,:,:]
+			x_t = x[t,:,:]
 			#Project input into the hidden vector space
 			z_t = self.project(x_t)
 			
 			#Combine input and hidden into gating variable k
-			combined = torch.cat((h_t,x),0)
+			combined = torch.cat((h_t,x_t),-1)
 			k_t = self.gate(combined)
 			#Control which information is preserved in the current step t
 			forget_x = k_t * z_t
@@ -28,9 +34,9 @@ class STAR_Single(nn.Module):
 			h_t = self.tanh_out(forget_x+forget_h)
 			
 			#Save h_t for the current step
-			hidden_out[:,t] = h_t
+			hidden_out[t,:] = h_t
 		
-		return h_t, hidden_out
+		return hidden_out, h_t
 			
 			
 			
